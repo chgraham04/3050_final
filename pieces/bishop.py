01b1509eaeec11a7c1383e73403d8d75490c73e6
kfoleyru <kfoleyru@uvm.edu>
2025-10-08 13:15:16 -0400

    Bishop get_moves

--- pieces/bishop.py
+++ pieces/bishop.py
@@ -23,38 +23,29 @@ class Bishop(Piece):
                      (-1, -1),
                      (-1, 1)]
 
-        # Bishop can move until it hits an edge or hits its same color piece
-        # Bishop can take any piece in a clear diagonal path
+        for i in range(len(move_list)):
+            x_pos, y_pos = move_list[i]
+            counter = 1
 
-        # Check for standard move
-        check_square = (position[0] + regular_move[0], position[1] + regular_move[1])
+            while True:
+                check_square = position[0] + x_pos * counter, position[1] + y_pos * counter
 
-        if 0 <= check_square[0] <= 7 and 0 <= check_square[1] <= 7:
-            tile = board.grid[check_square[0], check_square[1]]
+                # Ensure square is within bounds of board
+                if not (0 <= check_square[0] <= 7 and 0 <= check_square[1] <= 7):
+                    break
 
-            if not tile.has_piece():
-                legal_moves.append(check_square)
-
-                # Check for first move
-        if (self.has_moved == False):
-            check_square = (position[0] + first_move[0], position[1] + first_move[1])
-
-            if 0 <= check_square[0] <= 7 and 0 <= check_square[1] <= 7:
                 tile = board.grid[check_square[0], check_square[1]]
 
-                if not tile.has_piece():
+                if tile is not None:
+                    if tile.is_same_color():
+                        break
+                    else:
+                        legal_moves.append(check_square)
+                        break
+
+                else:
                     legal_moves.append(check_square)
 
-                    # Check for takes
-        for i in range(len(takes)):
-            check_square = (position[0] + takes[i][0], position[1] + takes[i][1])
-
-            # Ensure square is within bounds and not a friendly piece
-            if 0 <= check_square[0] <= 7 and 0 <= check_square[1] <= 7:
-                tile = board.grid[check_square[0], check_square[1]]
-
-                # See what piece is on the board; from tile.py
-                if tile.is_opposite_color(self.color):
-                    legal_moves.append((check_square))
+                counter += 1
 
         return legal_moves
