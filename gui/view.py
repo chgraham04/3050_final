67317e8c0e3b8d31144f89f92dd36f66efdb863d
Tim Smith <timothy.smith@uvm.edu>
2025-10-13 19:08:52 -0400

    Addded Sprites

--- gui/view.py
+++ gui/view.py
@@ -31,6 +31,7 @@ class GameView(arcade.View):
         super().__init__()
         arcade.set_background_color(C.CADET_BLUE)
 
+
         # assign board
         self.board = Board()
 
@@ -39,7 +40,44 @@ class GameView(arcade.View):
         self.origin_x = (width  - self.square * 8) // 2
         self.origin_y = (height - self.square * 8) // 2
 
+        #Sprites/pieces
+        self.sprites = arcade.SpriteList()
+        self.Wpawn1 = arcade.Sprite("C:\\Users\\Tim Smith\\PycharmProjects\\3050_final\\gui\\whitePawn.png", scale=.8, center_y=160, center_x=60)
+        self.Wpawn2 = arcade.Sprite("C:\\Users\\Tim Smith\\PycharmProjects\\3050_final\\gui\\whitePawn.png", scale=.8, center_y=160, center_x=160)
+        self.Wpawn3 = arcade.Sprite("C:\\Users\\Tim Smith\\PycharmProjects\\3050_final\\gui\\whitePawn.png", scale=.8, center_y=160, center_x=260)
+        self.Wpawn4 = arcade.Sprite("C:\\Users\\Tim Smith\\PycharmProjects\\3050_final\\gui\\whitePawn.png", scale=.8, center_y=160, center_x= 360)
+        self.Wpawn5 = arcade.Sprite("C:\\Users\\Tim Smith\\PycharmProjects\\3050_final\\gui\\whitePawn.png", scale=.8, center_y=160, center_x=465)
+        self.Wpawn6 = arcade.Sprite("C:\\Users\\Tim Smith\\PycharmProjects\\3050_final\\gui\\whitePawn.png", scale=.8, center_y=160, center_x=570)
+        self.Wpawn7 = arcade.Sprite("C:\\Users\\Tim Smith\\PycharmProjects\\3050_final\\gui\\whitePawn.png", scale=.8, center_y=160, center_x=670)
+        self.Wpawn8 = arcade.Sprite("C:\\Users\\Tim Smith\\PycharmProjects\\3050_final\\gui\\whitePawn.png", scale=.8, center_y=160, center_x=780)
+        self.sprites.append(self.Wpawn1)
+        self.sprites.append(self.Wpawn2)
+        self.sprites.append(self.Wpawn3)
+        self.sprites.append(self.Wpawn4)
+        self.sprites.append(self.Wpawn5)
+        self.sprites.append(self.Wpawn6)
+        self.sprites.append(self.Wpawn7)
+        self.sprites.append(self.Wpawn8)
+
+        self.Brook1 = arcade.Sprite("C:\\Users\\Tim Smith\\PycharmProjects\\3050_final\\gui\\blackRook.png", scale=.8, center_y=795, center_x=60)
+        self.Brook2 = arcade.Sprite("C:\\Users\\Tim Smith\\PycharmProjects\\3050_final\\gui\\blackRook.png", scale=.8, center_y=795, center_x=780)
+        self.sprites.append(self.Brook1)
+        self.sprites.append(self.Brook2)
+
+
+
+
+        # for i in range(7):
+        #     name = "pawn" + str(i)
+        #     x_coord = 60 + 100 * i
+        #     self.name = arcade.Sprite("C:\\Users\\Tim Smith\\PycharmProjects\\3050_final\\gui\\whitePawn.png", scale=.8, center_y=160, center_x=(x_coord))
+        #     self.sprites.append(self.name)
+
+
+
+
     def on_draw(self):
         # ensure nothing is overwritten without being cleared
         self.clear()
         draw_board(self.board, self.origin_x, self.origin_y, self.square)
+        self.sprites.draw()
