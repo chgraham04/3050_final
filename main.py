ffd291082c2bda887d715bc0aba616761bb3897d
timsmith2003 <148157082+timsmith2003@users.noreply.github.com>
2025-10-20 19:36:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,15 @@ from arcade import color as C
 from board.board import Board
 from pieces.piece import Piece, PieceType, Color
 from gui.view import GameView, draw_board
+from game.game import Game
 
 # constants
 # TODO: make these dynamic depending on device
-SCREEN_WIDTH = 850
-SCREEN_HEIGHT = 850
-SCREEN_TITLE = "CS3050 Chess"
+
 
 def main():
-    # --- BOARD TESTING --- #
-    # board = Board()
-    # board.print_board()
-    # --- END TESTING --- #
-    board.board_state()
-    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    view = GameView(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    window.show_view(view)
-    arcade.run()
-
+    game = Game()
+    game.play_game()
+    
 if __name__ == "__main__":
-    main()
+    main() 
