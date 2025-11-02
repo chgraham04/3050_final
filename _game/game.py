a527a9b4280d6ddf750924a1dbc3b3dc67fc38b3
chgraham04 <chgraham2004@gmail.com>
2025-11-02 14:52:34 -0500

    PYLINT: game.py: too few public methods, not sure how to fix

--- _game/game.py
+++ _game/game.py
@@ -1,9 +1,9 @@
-import arcade
+""" Game class creates and stores a board object, and turn tracker """
 from arcade import color as Color
 from _board.board import Board
 
 class Game:
+    """ Store board object and player turns for gameplay"""
     def __init__(self) -> None:
         self.board = Board()
         self.turn = Color.WHITE
-
