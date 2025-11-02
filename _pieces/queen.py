d143e8db9379d81dbc381dd9c0441396cdc8b24b
chgraham04 <chgraham2004@gmail.com>
2025-11-02 17:51:31 -0500

    PYLINT: queen passes

--- _pieces/queen.py
+++ _pieces/queen.py
@@ -1,23 +1,41 @@
+"""
+Models queen for game use
+"""
 from dataclasses import dataclass
-from _pieces.piece import Piece
 from _enums.piece_type import PieceType
 from _enums.color import Color
 from _enums.piece_value import PieceValue
+from _pieces.piece import Piece
 
 
 @dataclass
 class Queen(Piece):
+    """
+    Class representing the queen piece
+    """
     def __init__(self, color: Color, start_pos: tuple):
+        """
+        Initialize a queen piece
+
+        Args:
+            color: Color of the piece (White or Black)
+            start_pos: Starting position as (file, rank)
+        """
         super().__init__(PieceType.QUEEN, color, PieceValue.QUEEN, start_pos)
 
-    def get_position(self):
-        return super().get_position()
-    
     def get_moves(self, board) -> list[tuple[int, int]]:
+        """
+        Get all legal moves for the queen
+
+        Args:
+            board: The game board
+        Returns:
+            List of legal move positions as (file, rank) tuples
+        """
         legal_moves = []
         position = self.current_pos
 
-        #Potential moves
+        # Potential moves
         possibilities = [
             (0, 1),
             (0, -1),
@@ -29,14 +47,13 @@ class Queen(Piece):
             (-1, -1),
         ]
 
-        for i in range(len(possibilities)):
-            x_pos, y_pos = possibilities[i]
+        for x_pos, y_pos in possibilities:
             counter = 1
-           
+
             while True:
                 check_square = position[0] + x_pos * counter, position[1] + y_pos * counter
 
-                #Ensure square is within bounds of _board
+                # Ensure square is within bounds of board
                 if not (0 <= check_square[0] <= 7 and 0 <= check_square[1] <= 7):
                     break
 
@@ -45,14 +62,10 @@ class Queen(Piece):
                 if tile is not None and tile.has_piece():
                     if tile.is_same_color(self.color):
                         break
-                    else:
-                        legal_moves.append(check_square)
-                        break
-
-                else:
                     legal_moves.append(check_square)
+                    break
 
+                legal_moves.append(check_square)
                 counter += 1
 
         return legal_moves
-
