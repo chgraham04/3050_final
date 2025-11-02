27214b4fa0131d1cbd75bf63f82bde5fbec8275a
chgraham04 <chgraham2004@gmail.com>
2025-11-02 17:05:33 -0500

    PYLINT: bishop passes

--- _pieces/bishop.py
+++ _pieces/bishop.py
@@ -1,18 +1,36 @@
+"""
+Models bishop for game use
+"""
 from dataclasses import dataclass
-from _pieces.piece import Piece
 from _enums.piece_type import PieceType
 from _enums.color import Color
 from _enums.piece_value import PieceValue
+from _pieces.piece import Piece
 
 @dataclass
 class Bishop(Piece):
+    """
+    Class representing the bishop piece
+    """
     def __init__(self, color: Color, start_pos: tuple):
+        """
+        Initialize a bishop piece
+
+        Args:
+            color: Color of the piece (White or Black)
+            start_pos: Starting position as (file, rank)
+        """
         super().__init__(PieceType.BISHOP, color, PieceValue.BISHOP, start_pos)
 
-    def get_position(self):
-        return super().get_position()
-
     def get_moves(self, board) -> list[tuple[int, int]]:
+        """
+        Get all legal moves for the bishop
+
+        Args:
+            board: The game board
+        Returns:
+            List of legal move positions as (file, rank) tuples
+        """
         legal_moves = []
         position = self.current_pos
 
@@ -21,14 +39,13 @@ class Bishop(Piece):
                      (-1, -1),
                      (1, -1)]
 
-        for i in range(len(move_list)):
-            x_pos, y_pos = move_list[i]
+        for x_pos, y_pos in move_list:
             counter = 1
 
             while True:
                 check_square = position[0] + x_pos * counter, position[1] + y_pos * counter
 
-                # Ensure square is within bounds of _board
+                # Ensure square is within bounds of board
                 if not (0 <= check_square[0] <= 7 and 0 <= check_square[1] <= 7):
                     break
 
@@ -37,13 +54,10 @@ class Bishop(Piece):
                 if tile is not None and tile.has_piece():
                     if tile.is_same_color(self.color):
                         break
-                    else:
-                        legal_moves.append(check_square)
-                        break
-
-                else:
                     legal_moves.append(check_square)
+                    break
 
+                legal_moves.append(check_square)
                 counter += 1
 
         return legal_moves
