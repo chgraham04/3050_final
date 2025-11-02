da45b4447f8077d30c3fa058249312e54db7aa4f
chgraham04 <chgraham2004@gmail.com>
2025-11-02 12:07:18 -0500

    import_stockfish() called in bot.py to ensure accurate filepath

--- bot/bot.py
+++ bot/bot.py
@@ -1,17 +1,24 @@
+import os
 from enums.color import Color
 from stockfish import Stockfish
 from game.import_stockfish import import_stockfish
 
+
 class Bot:
     def __init__(self) -> None:
-        self.stockfish = Stockfish(path=r"C:\Users\darkf\Downloads\stockfish-windows-x86-64-avx2\stockfish\stockfish-windows-x86-64-avx2.exe")
+        stockfish_path = import_stockfish()
 
+        if stockfish_path is None:
+            raise FileNotFoundError("Could not find or download Stockfish executable")
+
+        print(f"Using Stockfish at: {stockfish_path}")
+        self.stockfish = Stockfish(path=stockfish_path)
         self.color = Color.BLACK
 
     def next_move(self, fen: str) -> list[tuple[int, int]]:
-        files = {"a" : 0, "b" : 1, "c" : 2,
-                 "d" : 3, "e" : 4, "f" : 5,
-                  "g" : 6, "h" : 7}
+        files = {"a": 0, "b": 1, "c": 2,
+                 "d": 3, "e": 4, "f": 5,
+                 "g": 6, "h": 7}
         position = self.stockfish.set_fen_position(fen)
         best_move = self.stockfish.get_best_move(position)
         print(self.stockfish.is_fen_valid(fen=fen))
@@ -20,4 +27,4 @@ class Bot:
         start_rank = best_move[1]
         move_to_file = files[best_move[2]]
         move_to_rank = best_move[3]
-        return[(int(start_rank)-1, start_file),(int(move_to_rank)-1, move_to_file)]
+        return [(int(start_rank) - 1, start_file), (int(move_to_rank) - 1, move_to_file)]
