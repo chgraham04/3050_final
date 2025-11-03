934c1df077f94e7ae3ab3deb003a474b8657bc2a
HaileyEmeline <hschoppe@uvm.edu>
2025-11-02 19:00:03 -0500

    stockfish works on mac

--- _game/import_stockfish.py
+++ _game/import_stockfish.py
@@ -5,6 +5,7 @@ from urllib.request import urlopen
 from zipfile import ZipFile
 from tarfile import open as taropen
 from io import BytesIO
+from stockfish import Stockfish
 
 #Function automatically downloads and opens stockfish chess engine
 def import_stockfish(dir_path = "_assets/stockfish"):
@@ -23,11 +24,10 @@ def import_stockfish(dir_path = "_assets/stockfish"):
 
                     if os.access(path_to_stockfish, os.X_OK):
                         return path_to_stockfish
-                
+               
     #If not installed, install it
     if not stockfish_found:
         print("Not found")
-
         context = ssl._create_unverified_context()
         os.makedirs(dir_path, exist_ok=True)
 
@@ -46,11 +46,12 @@ def import_stockfish(dir_path = "_assets/stockfish"):
         elif (system == "Darwin"):
             file_path = "https://github.com/official-stockfish/Stockfish/releases/latest/download/stockfish-macos-m1-apple-silicon.tar"
             zip_file = False
-        
+       
         else:
             raise Exception(f"Unsupported OS: {system}")
 
         try:
+            
             #Open and read download link
             opened_file = urlopen(file_path, context=context)
             file_data = BytesIO(opened_file.read())
@@ -59,7 +60,7 @@ def import_stockfish(dir_path = "_assets/stockfish"):
             if (zip_file):
                 with ZipFile(file_data) as zip_file_obj:
                     zip_file_obj.extractall(path=dir_path)
-            
+           
             else:
                 with taropen(fileobj = file_data) as tar_file:
                     tar_file.extractall(path=dir_path)
@@ -73,14 +74,13 @@ def import_stockfish(dir_path = "_assets/stockfish"):
                         if system != "Windows" and not os.access(path_to_stockfish, os.X_OK):
                             print("Making file executable")
                             os.chmod(path_to_stockfish, 0o755)
-                        
+                       
                         return path_to_stockfish
-            
+           
             print("Stockfish could not be installed")
             return None
-            
+           
 
         except FileNotFoundError:
             print("File could not be found")
             return None
-
